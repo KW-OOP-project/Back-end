80840e270f95481a5085f992b62cf93458bbe8be
seyeon22 <sywolleych@gmail.com>
2024-10-14 16:21:06 +0900

    dto 추가

--- src/main/java/com/springboot/harubi/Service/PlanService.java
+++ src/main/java/com/springboot/harubi/Service/PlanService.java
@@ -1,4 +1,9 @@
 package com.springboot.harubi.Service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class PlanService {
 }
